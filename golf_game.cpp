7895a72725576ef1cc34683dd8c1b7d00e090975
praniil <pparajuli15@gmail.com>
2024-05-28 16:49:20 +0545

    feat: added motion to the golf ball opposite to the mouse drag and also implemented friction

--- golf_game.cpp
+++ golf_game.cpp
@@ -1,19 +1,22 @@
 #include<SFML/Graphics.hpp>
 #include "golf_game.h"
-
+#include<stdio.h>
 #include<iostream>
 
 //golbal variable
 int golf_ball_diameter = 10;
+const float dragscale = 0.2f;
+const float damping = 0.99f;
 
 Game :: Game() {
     //game window config
     sf::VideoMode desktop_size = sf::VideoMode::getDesktopMode();
-    game_window.create(desktop_size, "GOLF_GAME");
+    game_window.create(desktop_size, "GOLF_GAME", sf::Style::Fullscreen);
+    game_window.setFramerateLimit(100);
     //golf ball attribute
     golf_ball.setRadius(golf_ball_diameter);
     golf_ball.setFillColor(sf::Color::White);
-    golf_ball.setPointCount(100000);
+    golf_ball.setPointCount(30);
 
     //golf ball position
     golf_ball_pos_x = 5 * golf_ball_diameter;
@@ -23,13 +26,22 @@ Game :: Game() {
     //golf hole attribute
     golf_hole.setRadius(golf_ball_diameter * 1.52);
     golf_hole.setFillColor(sf::Color::Black);
-    golf_hole.setPointCount(100000);
-
+    golf_hole.setPointCount(30);
 
     //golf hole position
     golf_hole_pos_x = game_window.getSize().x /2 + golf_ball_diameter;
     golf_ball_pos_y = 3 * golf_ball_diameter;
     golf_hole.setPosition(golf_hole_pos_x, golf_ball_pos_y);
+
+    //arrow attribut
+    // arrow.setSize(sf::Vector2f(5.0, 0.5));
+    // arrow.setFillColor(sf::Color::Black);
+
+    // //arrow positioin
+    // arrow.setPosition(golf_ball.getPosition().x + 20.f, golf_ball.getPosition().y + 20.f);
+    //dragging attribute
+    isDragging = false;
+
 }
 
 bool Game::isRunning() const{
@@ -40,25 +52,64 @@ void Game::run() {
     while(game_window.isOpen()) {
         sf::Event event;
         while(game_window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                game_window.close();
-            }
-
-            if(event.type == sf::Event::KeyPressed ) {
-                if(event.key.code == sf::Keyboard::Escape) {
+                if (event.type == sf::Event::Closed) {
                     game_window.close();
-                    std::cout<<"in event" << std::endl;
                 }
-            }
+
+                if(event.type == sf::Event::KeyPressed ) {
+                    if(event.key.code == sf::Keyboard::Escape) {
+                        game_window.close();
+                        std::cout<<"in event" << std::endl;
+                    }
+                }
+                
+                //for getting the start position of ball
+                if(event.type == sf::Event::MouseButtonPressed) {
+                    if (event.mouseButton.button == sf::Mouse::Left) {
+                        sf::Vector2f mousePos(event.mouseButton.x, event.mouseButton.y);
+                        printf("mouse pos x: %f", mousePos.x);
+                        if (golf_ball.getGlobalBounds().contains(mousePos)) {
+                            isDragging = true;
+                            dragStartPos = mousePos;
+                            printf("drag posn x: %f", dragStartPos.x);
+                        }
+                    }
+                }
+                
+                //for getting the end position of the ball after drag
+                if (event.type == sf::Event::MouseButtonReleased) {
+                    if (event.mouseButton.button == sf::Mouse::Left) {
+                        if (isDragging) {
+                            dragEndPos = sf::Vector2f(event.mouseButton.x, event.mouseButton.y);
+                            printf("drag end pos: %f", dragEndPos.x); 
+                            sf::Vector2f distance_drag = dragEndPos - dragStartPos;
+                            golf_ball_velocity = -distance_drag * dragscale;
+                            isDragging = false;
+                        }
+                    }
+                }
         } 
-        render();
+            //updating the position of the ball
+            update();
+            render();
+    }
+}
+
+void Game::update() {
+    if(!isDragging) {
+        printf("1");
+        golf_ball.move(golf_ball_velocity);
+        printf("2");
+        golf_ball_velocity *= damping;
     }
 }
 
 void Game::render() {
+    printf("3");
     game_window.clear(sf::Color::Green);
     game_window.draw(golf_ball);
     game_window.draw(golf_hole);
+    // game_window.draw(arrow);
     game_window.display();
 }
 
--- golf_game.h
+++ golf_game.h
@@ -7,14 +7,27 @@ class Game{
         bool isRunning() const;
         void run();
         void render();
+        void update();
     
     private:
         //window
         sf::RenderWindow game_window;
+
         //golf ball attributes
         sf::CircleShape golf_ball;
         int golf_ball_pos_x, golf_ball_pos_y;
+
         //golf hole attribute
         sf::CircleShape golf_hole;
         int golf_hole_pos_x, golf_hole_pos_y;
+
+        //arrow
+        // sf::RectangleShape arrow;
+
+        //dragging
+        bool isDragging;
+        sf::Vector2f dragStartPos;
+        sf::Vector2f dragEndPos;
+        sf::Vector2f golf_ball_velocity;
+        
 };
